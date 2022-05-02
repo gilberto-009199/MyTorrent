05b1791fa0cc7c33e7db78c728f49dd37bb05b64
administrador <gilberto.tec@vivaldi.net>
2022-05-02 16:06:43 -0300

    leitor bencode

--- src/main/java/org/voyager/torrent/client/ClientTorrent.java
+++ src/main/java/org/voyager/torrent/client/ClientTorrent.java
@@ -1,5 +1,15 @@
 package org.voyager.torrent.client;
 
-public class ClientTorrent {
+import java.io.File;
 
+public class ClientTorrent { 
+	
+	public static String separator = System.getProperty("file.separator");
+	public static String dirUser = new File(System.getProperty("user.home")).getAbsolutePath()+separator;
+	public static String dirRuntime = "."+separator;
+	
+	
+	public ClientTorrent Build() {
+		return null;
+   }
 }
